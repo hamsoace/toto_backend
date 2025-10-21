bcfb5510f3ac075c4ac047cc7f2e68ceb770eadd
cuisinemem <cuisinemem@gmail.com>
2025-10-21 08:43:05 +0000

    changed import to task routes

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
-const taskController = require('../controllers/taskController');
+const { getTasks, createTask, updateTask, deleteTask } = require('../controllers/taskController');
 
-router.get('/', auth, taskController.getTasks);
-router.post('/', auth, taskController.createTask);
-router.put('/:id', auth, taskController.updateTask);
-router.delete('/:id', auth, taskController.deleteTask);
+router.get('/', auth, getTasks);
+router.post('/', auth, createTask);
+router.put('/:id', auth, updateTask);
+router.delete('/:id', auth, deleteTask);
 
 module.exports = router;
